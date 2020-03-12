5d11849e3d9eeda00ffaa97665f01faf497591e6
Diego Rocchini <roquinni@gmail.com>
2020-03-12 19:12:05 -0300

    Adding application

---
+++ source/AngularCqrsCore.Application/Common/Behaviours/RequestLogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Common.Behaviours
+{
+    public class RequestLogger<TRequest> : IRequestPreProcessor<TRequest>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public RequestLogger(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name;
+            _logger.LogInformation("CleanArchitecture Request: {Name} {@Request}", requestName, request);
+        }
+    }
+    }
