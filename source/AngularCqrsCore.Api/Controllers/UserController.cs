c7293ff05851319bd6400e7aad4aa16957d98f0e
Diego Rocchini <roquinni@gmail.com>
2020-03-13 11:46:01 -0300

    Changing web project name to Api

---
+++ source/AngularCqrsCore.Api/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Users.Queries.GetAll;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : BaseController
+    {
+        public UserController()
+        {
+                
+        }
+
+
+        // GET: api/User
+        [HttpGet]
+        public async Task<UsersVm> Get()
+        {
+            var users = await Mediator.Send(new GetAllQuery());
+            return users;
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
