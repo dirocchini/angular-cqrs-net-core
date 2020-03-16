e0d2b2365590bb2555326b71d59ca43d22845279
Diego Rocchini <roquinni@gmail.com>
2020-03-15 23:56:54 -0300

    Login

--- source/WebSpa/src/app/nav/nav.component.ts
+++ source/WebSpa/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,13 +10,16 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
-  login(){
-    console.log(this.model);
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('login ok');
+    }, error => {
+      console.log('login failed');
+    });
   }
-
 }
