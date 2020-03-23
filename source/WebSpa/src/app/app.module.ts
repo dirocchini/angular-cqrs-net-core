b324d74c321f71f7af1350744193858a09dbd5c6
Diego Rocchini <roquinni@gmail.com>
2020-03-23 18:40:04 -0300

    Added User details engine

--- source/WebSpa/src/app/app.module.ts
+++ source/WebSpa/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { appRoutes } from './routes';
 import { MembersCardComponent } from './members/members-card/members-card.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { UserService } from './_services/user.service';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -34,7 +35,8 @@ export function tokenGetter() {
       MemberListComponent,
       ListsComponent,
       MessagesComponent,
-      MembersCardComponent
+      MembersCardComponent,
+      MemberDetailComponent
    ],
    imports: [
       BrowserModule,
