ec086813b2b881c7fa8b3546f5b16709c23a0209
Diego Rocchini <roquinni@gmail.com>
2020-04-03 11:03:47 -0300

    Added Behavior Subject

--- source/WebSpa/src/app/nav/nav.component.ts
+++ source/WebSpa/src/app/nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { Router } from '@angular/router';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
    selector: 'app-nav',
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
 })
 export class NavComponent implements OnInit {
    model: any = {};
+   photoUrl: string;
 
    constructor(
       public authService: AuthService,
@@ -17,7 +18,11 @@ export class NavComponent implements OnInit {
       private router: Router
    ) {}
 
-   ngOnInit() {}
+   ngOnInit() {
+      this.authService.currentPhotoUrl.subscribe(
+         photo => (this.photoUrl = photo)
+      );
+   }
 
    login() {
       this.authService.login(this.model).subscribe(
