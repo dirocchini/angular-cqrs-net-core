b00630595ce3960021345f288ecf72400553c622
Diego Rocchini <roquinni@gmail.com>
2020-04-06 14:55:22 -0300

    Reactive Form Created For Registration

--- source/WebSpa/src/app/register/register.component.ts
+++ source/WebSpa/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
    selector: 'app-register',
@@ -12,20 +13,33 @@ export class RegisterComponent implements OnInit {
 
    model: any = {};
 
-   constructor(private authService: AuthService, private alertify: AlertifyService) {}
+   registerForm: FormGroup;
 
-   ngOnInit() {}
+   constructor(
+      private authService: AuthService,
+      private alertify: AlertifyService
+   ) {}
+
+   ngOnInit() {
+      this.registerForm = new FormGroup({
+         name: new FormControl(),
+         login: new FormControl(),
+         password: new FormControl(),
+         confirmPassword: new FormControl()
+      });
+   }
 
    register() {
-      //  console.log(this.model);
-      this.authService.register(this.model).subscribe(
-         () => {
-            this.alertify.success('Registered successfully');
-         },
-         error => {
-            this.alertify.error(error);
-         }
-      );
+      console.log(this.registerForm.value);
+      // //  console.log(this.model);
+      // this.authService.register(this.model).subscribe(
+      //    () => {
+      //       this.alertify.success('Registered successfully');
+      //    },
+      //    error => {
+      //       this.alertify.error(error);
+      //    }
+      // );
    }
 
    cancel() {
