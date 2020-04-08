f900a3362133bf400864137618196665d42bc481
Diego Rocchini <roquinni@gmail.com>
2020-04-08 13:15:26 -0300

    Filter Api by Age Enhanced

---
+++ source/AngularCqrsCore.Infrastructure/Migrations/20200408161300_resolved-age.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class resolvedage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Users");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2020, 4, 8, 13, 12, 59, 477, DateTimeKind.Local).AddTicks(5846));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2020, 3, 28, 10, 32, 43, 662, DateTimeKind.Local).AddTicks(7646));
+        }
+    }
+}
