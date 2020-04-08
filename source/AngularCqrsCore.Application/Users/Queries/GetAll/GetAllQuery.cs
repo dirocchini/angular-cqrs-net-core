7c1f892324e245bbc63333e917f57d5c05e0e5f0
Diego Rocchini <roquinni@gmail.com>
2020-04-08 13:10:21 -0300

    Filter Api by Age

--- source/AngularCqrsCore.Application/Users/Queries/GetAll/GetAllQuery.cs
+++ source/AngularCqrsCore.Application/Users/Queries/GetAll/GetAllQuery.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,15 +14,20 @@ namespace Application.Users.Queries.GetAll
 {
     public class GetAllQuery : IRequest<UserVMPagination>
     {
-        private const int MaxPageSize = 6;
+        public int CurrentUserId { get; set; }
+        public int Age { get; set; }
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 99;
 
+        private const int MaxPageSize = 6;
         public int PageNumber { get; set; } = 1;
         public int PageSize { get; set; }
 
         public string Gender { get; set; }
-        public int Age { get; set; }
+        
+        
 
-        public int CurrentUserId { get; set; }
+
 
         public class GetAllQueryHandler : IRequestHandler<GetAllQuery, UserVMPagination>
         {
@@ -52,6 +58,15 @@ namespace Application.Users.Queries.GetAll
                 var users = _applicationContext.Users.Include(u => u.Photos).AsQueryable();
                 users = users.Where(u => u.Id != request.CurrentUserId);
                 users = users.Where(u => u.Gender.ToLower().Trim() == request.Gender.ToLower().Trim());
+
+                if(request.MinAge != 18 || request.MaxAge != 99)
+                {
+                    var minDob = DateTime.Today.AddYears(-request.MaxAge -1);
+                    var maxDob = DateTime.Today.AddYears(-request.MinAge);
+                    users = users.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+                }
+
+
 
                 var pagedList = await PagedList<User>.CreateAsync(users, request.PageNumber, request.PageSize);
 
