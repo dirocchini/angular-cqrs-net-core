e1254a193cd3eae500865a7dcc7afef45d7659db
Diego Rocchini <roquinni@gmail.com>
2020-04-18 18:41:46 -0300

    Hidding Menu

--- source/WebSpa/src/app/app.module.ts
+++ source/WebSpa/src/app/app.module.ts
@@ -40,6 +40,7 @@ import { ListsResolver } from './_resolvers/lists.resolver';
 import { MessagesResolver } from './_resolvers/messages.resolver';
 import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { HasRoleDirective } from './_directives/hasRole.directive';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -67,7 +68,8 @@ const jwtConf = {
       MemberEditComponent,
       PhotoEditorComponent,
       MemberMessagesComponent,
-      AdminPanelComponent
+      AdminPanelComponent,
+      HasRoleDirective
    ],
    imports: [
       BrowserModule,
