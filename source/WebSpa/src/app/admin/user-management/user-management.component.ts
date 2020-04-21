89d9ad48ba0709cf2928fe20eb269a3e1f8633b6
Diego Rocchini <roquinni@gmail.com>
2020-04-21 14:43:06 -0300

    Edit Roles

--- source/WebSpa/src/app/admin/user-management/user-management.component.ts
+++ source/WebSpa/src/app/admin/user-management/user-management.component.ts
@@ -3,6 +3,7 @@ import { User } from 'src/app/_models/user';
 import { AdminService } from 'src/app/_services/admin.service';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap';
 import { RolesModalComponent } from '../roles-modal/roles-modal.component';
+import { ValueTransformer } from '@angular/compiler/src/util';
 
 @Component({
    selector: 'app-user-management',
@@ -33,19 +34,64 @@ export class UserManagementComponent implements OnInit {
       );
    }
 
-   editRolesModal() {
+   editRolesModal(user: User) {
       const initialState = {
-         list: [
-            'Open a modal with component',
-            'Pass your data',
-            'Do something else',
-            '...',
-         ],
-         title: 'Modal with component',
+         user,
+         roles: this.getRoles(user),
       };
       this.bsModalRef = this.modalService.show(RolesModalComponent, {
          initialState,
       });
-      this.bsModalRef.content.closeBtnName = 'Close';
+      this.bsModalRef.content.updateSelectedRoles.subscribe((value) => {
+         const rolesToUpdate = {
+            roleNames: [
+               ...value
+                  .filter((el) => el.checked === true)
+                  .map((el) => el.name),
+            ],
+         };
+
+
+         if (rolesToUpdate) {
+            this.adminService.updateUserRoles(user, rolesToUpdate).subscribe(
+               () => {
+                  user.roles = [...rolesToUpdate.roleNames];
+               },
+               (error) => {
+                  console.log(error);
+               }
+            );
+         }
+      });
+   }
+
+   private getRoles(user) {
+      const roles = [];
+      const userRoles = user.roles;
+      const availableRoles: any[] = [
+         { name: 'Admin', value: 'Admin' },
+         { name: 'Moderator', value: 'Moderator' },
+         { name: 'Member', value: 'Member' },
+         { name: 'VIP', value: 'VIP' },
+      ];
+
+      // tslint:disable-next-line: prefer-for-of
+      for (let i = 0; i < availableRoles.length; i++) {
+         let isMatch = false;
+         // tslint:disable-next-line: prefer-for-of
+         for (let j = 0; j < userRoles.length; j++) {
+            if (availableRoles[i].name === userRoles[j]) {
+               isMatch = true;
+               availableRoles[i].checked = true;
+               roles.push(availableRoles[i]);
+               break;
+            }
+         }
+         if (!isMatch) {
+            availableRoles[i].checked = false;
+            roles.push(availableRoles[i]);
+         }
+      }
+      return roles;
    }
 }
